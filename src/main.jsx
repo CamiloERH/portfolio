0b60ba7f1e6222380552b1696fd9be08ad40512c
Camilo Rojas <71569364+CamiloERH@users.noreply.github.com>
2023-11-28 02:31:42 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import AppContainer from './AppContainer.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/portfolio/">
+  <HashRouter>
     <AppContainer />
-  </BrowserRouter>
+  </HashRouter>
 )
